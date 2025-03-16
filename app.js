d30e3799f87843378d4d9633f073b806f9b0db6b
Kashansaleem192 <kashansaleem192@gmail.com>
2025-03-16 06:44:36 +0500

    Assignment 6

---
+++ app.js
@@ -0,0 +1,59 @@
+// Asignment 21-25
+
+// // Q 1
+// var firstName = prompt("Enter Your First Name")
+// var LastName = prompt("Enter Your Last Name")
+// var fullName = alert( "Hi" + firstName, " ",LastName , "Welcome to our Website" )
+
+// // Q 2
+// var favourite_mobiles = prompt( "Enter Your Favourite Mobile Brands")
+// console.log("My Favourite Phones  is :" , favourite_mobiles)
+// var stringLength =favourite_mobiles.length
+// console.log( "String Length", stringLength)
+
+// // Q 3
+// var string = "Pakistani"
+// var index = string.indexOf("n");
+// console.log( "String", string  )
+// console.log(  "Index Number"+index
+// )
+
+// // Q 4
+
+// var String1 = "Hello World"
+// var IndexNum = String1.lastIndexOf("l")
+// console.log( "String", String1  )
+// console.log( "Index Number",IndexNum)
+
+// // Q 5
+// var string = "Pakistani"
+// var Char3 = string.charAt(3)
+
+// console.log( "String", string  )
+// console.log(  "Character at index 3 is", Char3)
+
+// Q 6
+// var firstName = prompt("Enter Your First Name")
+// var LastName = prompt("Enter Your Last Name")
+// var fullNames = firstName.concat(" " , LastName)
+// console.log( fullNames ,"Welcome to our Website")
+
+// Q 7
+
+var word = "Hyedarabad"
+var afterReplace = word.replace("Hyedar", "Islam")
+console.log("City ", word);
+console.log("After Replace", afterReplace)
+
+// Q 8
+
+var message = "Ali and Sami are best friends. They play cricket and football together."
+var replaceWord = message.replaceAll("and ", " & ")
+console.log(message)
+console.log("After Replace ", replaceWord)
+
+// Q 9
+var value = "472"
+var newvalue = 472
+console.log("Value", typeof value )
+console.log( "Value",typeof newvalue)
